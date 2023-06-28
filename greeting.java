8edfdabb1d42b888922f91d222b7899b2f835e9f
Suryansh <34952630+suryanshgr22@users.noreply.github.com>
2023-06-28 15:25:36 +0530

    Create greeting.java
    ASSIGNMENT 1.2

---
+++ greeting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String n = input.nextLine();
+        System.out.println("hello " + n);
+
+
+    }
+}
